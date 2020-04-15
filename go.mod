73caa7094f3410b903db20d18cbb6d4cf7418d53
brian <mr.brianhobo@gmail.com>
2020-04-15 00:47:48 -0700

    get web scraping to work

---
+++ go.mod
@@ -0,0 +1,15 @@
+module sliver.today
+
+go 1.13
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.1 // indirect
+	github.com/antchfx/htmlquery v1.2.2 // indirect
+	github.com/antchfx/xmlquery v1.2.3 // indirect
+	github.com/antchfx/xpath v1.1.5 // indirect
+	github.com/gocolly/colly/v2 v2.0.1
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.4.0 // indirect
+	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
+	google.golang.org/appengine v1.6.5 // indirect
+)
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/gocolly/colly/v2"
+)
+
+const (
+	// Sliver URLs to crawl
+	telegraphURL = "https://www.sliverpizzeria.com/pizza-telegraph/"
+	shattuckURL  = "https://www.sliverpizzeria.com/pizza-shattuck/"
+	broadwayURL  = "https://www.sliverpizzeria.com/pizza-broadway/"
+
+	// Today's Pizza is stored at index 2 of the query for "div .summary-excerpt p"
+	todaysPizzaIndex = 2
+)
+
+func trimStr(in string) string {
+	space := regexp.MustCompile(`\s+`)
+	out := space.ReplaceAllString(in, " ")
+	return out
+}
+
+func main() {
+	var todaysPizza string
+
+	// Create a collector
+	c := colly.NewCollector()
+
+	// Set HTML callback
+	c.OnHTML("div .summary-excerpt p", func(e *colly.HTMLElement) {
+		fmt.Println(e)
+		if e.Index == todaysPizzaIndex {
+			todaysPizza = e.Text
+		}
+	})
+
+	// Set error handler
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	})
+
+	// Start scraping
+	c.Visit(telegraphURL)
+
+	fmt.Printf("today's sliver pizza is: %s\n", trimStr(todaysPizza))
+}
